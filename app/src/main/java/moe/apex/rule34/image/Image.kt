38833044b7a8f8e8743c010adfd89aba74427652
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2024-08-10 23:27:37 +0100

    Simplify Image

--- app/src/main/java/moe/apex/rule34/image/Image.kt
+++ app/src/main/java/moe/apex/rule34/image/Image.kt
@@ -1,61 +1,19 @@
 package moe.apex.rule34.image
 
-import android.os.Parcel
-import android.os.Parcelable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-class Image(
+data class Image(
     val fileName: String,
     val fileFormat: String,
     val previewUrl: String,
     val fileUrl: String,
     val sampleUrl: String
-) : Parcelable {
-    val defaultUrl = sampleUrl.takeIf { it.isNotEmpty() } ?: fileUrl
-    val hdAvailable = sampleUrl.isNotEmpty() && fileUrl.isNotEmpty() && sampleUrl != fileUrl
+) {
     val highestQualityFormatUrl = fileUrl.takeIf { it.isNotEmpty() } ?: sampleUrl
     var preferHd by mutableStateOf(false)
     var hdQualityOverride: Boolean? by mutableStateOf(null)
-
-    constructor(parcel: Parcel) : this(
-        parcel.nextString(),
-        parcel.nextString(),
-        parcel.nextString(),
-        parcel.nextString(),
-        parcel.nextString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(fileName)
-        parcel.writeString(fileFormat)
-        parcel.writeString(previewUrl)
-        parcel.writeString(fileUrl)
-        parcel.writeString(sampleUrl)
-    }
-
-    override fun describeContents() = 0
-
-    companion object CREATOR : Parcelable.Creator<Image> {
-        override fun createFromParcel(parcel: Parcel): Image {
-            return Image(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Image?> {
-            return arrayOfNulls(size)
-        }
-
-        fun Parcel.nextString(): String = this.readString()!!
-    }
-
-    override fun hashCode(): Int {
-        return defaultUrl.hashCode()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is Image && other.defaultUrl == defaultUrl
-    }
 
     fun toggleHd(to: Boolean? = null) {
         preferHd = when (to) {
