a974f0371f992895e844656e5a36c27427b1e86b
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2025-05-24 00:27:44 +0100

    import

--- app/src/main/java/moe/apex/rule34/largeimageview/LargeImageView.kt
+++ app/src/main/java/moe/apex/rule34/largeimageview/LargeImageView.kt
@@ -64,7 +64,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
