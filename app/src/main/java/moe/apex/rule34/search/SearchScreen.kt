b1ca883ba4ae2f34b9e05fd6c9119e5a72138ecd
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2025-06-29 00:08:39 +0100

    import

--- app/src/main/java/moe/apex/rule34/search/SearchScreen.kt
+++ app/src/main/java/moe/apex/rule34/search/SearchScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
