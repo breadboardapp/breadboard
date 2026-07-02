9d8b1f9fab5768288a62038838e96fdbaa985055
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2026-07-02 09:04:53 +0100

    import

--- app/src/main/java/moe/apex/breadboard/navigation/Navigation.kt
+++ app/src/main/java/moe/apex/breadboard/navigation/Navigation.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
