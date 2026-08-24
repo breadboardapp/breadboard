3cd68b39b843bae1862cccfac3c030151a746847
flazepe <51487578+flazepe@users.noreply.github.com>
2026-08-24 21:11:16 +0800

    Remove unused import

--- app/src/main/java/moe/apex/breadboard/MainActivity.kt
+++ app/src/main/java/moe/apex/breadboard/MainActivity.kt
@@ -1,7 +1,6 @@
 package moe.apex.breadboard
 
 import android.app.UiModeManager
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
